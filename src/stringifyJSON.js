755bffe6efdf03fcd3c6e4bbb29581e070e1d1d0
Andrew Teich <hackreactor@Macintosh-4.local>
2014-08-04 13:41:32 -0400

    stringify works until test case [8]

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,37 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log("Should be", JSON.stringify(obj));
+
+  var toReturn = '';
+
+  //String Case
+  if(typeof obj == 'string' || obj instanceof String)
+  {
+  	console.log("Is ", '"' + obj + '"');
+	return '"' + obj + '"';
+  }
+
+  //Array Case
+  if(typeof obj == 'array' || obj instanceof Array)
+  {
+  	if(obj.length > 0)
+  	{
+  		toReturn += removeOutsideBrackets(stringifyJSON(obj.pop()));
+  		toReturn += removeOutsideBrackets(stringifyJSON(obj));
+  	}
+
+  	console.log("Is ", '[' + toReturn + ']');
+  	return '[' + toReturn + ']';
+  }
+
+  //DEFAULT
+  console.log("Is ", '' + obj);
+  return '' + obj;
+
+  function removeOutsideBrackets(string)
+  {
+  	var returnString = string.replace('[', "");
+  	return returnString.replace(/]$/, '');
+  }
 };
